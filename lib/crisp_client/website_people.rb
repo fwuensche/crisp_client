ae28391c54193c493295f44149ff1a5f356686e9
Flavio Wuensche <f.wuensche@gmail.com>
2017-06-08 20:14:26 +0200

    List people profiles

--- lib/crisp_client/website_people.rb
+++ lib/crisp_client/website_people.rb
@@ -2,4 +2,8 @@ module WebsitePeople
   def get_people_statistics(website_id)
     client_get("/website/#{website_id}/people/stats")["data"]
   end
+
+  def list_people_profiles(website_id, page_number = 1)
+    client_get("/website/#{website_id}/people/profiles/#{page_number}")["data"]
+  end
 end
